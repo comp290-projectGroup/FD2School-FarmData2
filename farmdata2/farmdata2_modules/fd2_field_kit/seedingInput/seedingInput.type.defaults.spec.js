964487271c707802cf94a1b6cb9ec5ff4925e430
hutterfly <huttere@dickinsno.edu>
2023-10-31 14:40:26 -0400

    added test to check that the tray and direct elements are not selected and that there is a message present to tell you to select one

--- farmdata2/farmdata2_modules/fd2_field_kit/seedingInput/seedingInput.type.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_field_kit/seedingInput/seedingInput.type.defaults.spec.js
@@ -9,4 +9,9 @@ describe("Ensure Seeding Input Log has sane default values", () => {
     it("Direct seeding is enabled", () => {
         cy.get("[data-cy='direct-seedings']").should('be.enabled')
     })
+    it("Tray and Direct elements not selected and message visible", () => {
+        cy.get("[data-cy='tray-seedings']").should('not.be.checked')
+        cy.get("[data-cy='direct-seedings']").should('not.be.checked')
+        cy.contains("Please Select Tray Seeding or Direct Seeding")
+    })
 })
