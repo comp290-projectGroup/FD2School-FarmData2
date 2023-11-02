c28e780bbb0f8369729c92ed4a6f0729b692aea3
VPhung1901 <phungv@dickinson.edu>
2023-11-02 17:22:43 -0400

    Test Seeding Input Type Defaults-13.
    Co-authored-by: Giang <nguyeng@dickinson.edu>
    Co-authored-by: Hutter <huttere@dickinson.edu>
    Co-authored-by: Vy <phungv@dickinson.edu>

--- farmdata2/farmdata2_modules/fd2_field_kit/seedingInput/seedingInput.type.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_field_kit/seedingInput/seedingInput.type.defaults.spec.js
@@ -1,17 +1,38 @@
-describe("Ensure Seeding Input Log has sane default values", () => {
+describe("Ensure Seeding Input Log has same default values", () => {
     beforeEach(() => {
         cy.login("manager1", "farmdata2")
         cy.visit("/farm/fd2-field-kit/seedingInput")
     })
+
+    /** Check elements for "Tray" and "Direct" are enabled. */
     it("Tray seeding is enabled", () => {
         cy.get("[data-cy='tray-seedings']").should('be.enabled')
     })
     it("Direct seeding is enabled", () => {
         cy.get("[data-cy='direct-seedings']").should('be.enabled')
     })
+
+    /** Check neither Tray nor Direct is selected and
+     *  a message is visible indicating that Tray or Direct must be selected. */
     it("Tray and Direct elements not selected and message visible", () => {
         cy.get("[data-cy='tray-seedings']").should('not.be.checked')
         cy.get("[data-cy='direct-seedings']").should('not.be.checked')
         cy.contains("Please Select Tray Seeding or Direct Seeding")
     })
+
+    /** Check the form elements for either the "Tray" and 
+    "Direct" seedings are not visible or do not exist. */
+
+    it("Check form elements of Tray is not visible or not exist", () => { 
+        cy.get("[data-cy=tray-area-selection]").should("not.be.visible")
+        cy.get("[data-cy=num-cell-input]").should("not.be.visible")
+        cy.get("[data-cy=num-tray-input]").should("not.be.visible")
+        cy.get("[data-cy=num-seed-input]").should("not.be.visible")
+    }) 
+    it("Check form elements of Direct is not visible or not exist", () => { 
+        cy.get("[data-cy=direct-area-selection]").should("not.be.visible")
+        cy.get("[data-cy=num-rowbed-input]").should("not.be.visible")
+        cy.get("[data-cy=unit-feet]").should("not.be.visible")
+        cy.get("[data-cy=num-feet-input]").should("not.be.visible")
+    }) 
 })
