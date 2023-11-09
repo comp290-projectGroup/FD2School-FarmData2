933a42c2d66ca277f29806bb75055391060310bf
VPhung1901 <phungv@dickinson.edu>
2023-11-09 18:57:21 -0500

    check selected crop table seeding logs

--- farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.cropFilter.spec.js
+++ farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.cropFilter.spec.js
@@ -2,7 +2,11 @@ describe("Testing crop filter for seeding report", () => {
     beforeEach(() => {
         cy.login("manager1", "farmdata2")
         cy.visit("/farm/fd2-barn-kit/seedingReport")
+        //cy.waitForPage()
     })
+
+    /**when “All” is selected in the Crop filter, the table will 
+     * contain logs for seedings for multiple different crops. */
     it("When All is selected in crop filter there will be logs for multiple different crops", () => {
         cy.get('[data-cy=start-date-select] > [data-cy=date-select]').type('2020-01-01').blur()
         cy.get('[data-cy=end-date-select] > [data-cy=date-select]').type('2020-12-01').blur()
@@ -14,4 +18,20 @@ describe("Testing crop filter for seeding report", () => {
         cy.get('[data-cy=td-r3c1]').contains('COLLARDS')
         cy.get('[data-cy=td-r4c1]').contains('HERB-MARJORAM')
     })
+
+    /**when a specific crop is selected in the Crop filter, 
+     * the table will contain only seeding logs for the selected type of crop. */
+    it("check table contains only selected crop's seeding logs", () => {
+        cy.get('[data-cy=start-date-select] > [data-cy=date-select]').type('2020-01-01').blur()
+        cy.get('[data-cy=end-date-select] > [data-cy=date-select]').type('2020-12-01').blur()
+        cy.get('[data-cy=generate-rpt-btn]').click()
+        //select crop ARUGULA which is the second crop row
+        cy.get('[data-cy=crop-dropdown] > [data-cy=dropdown-input] > [data-cy=option1]')
+
+        //check each row has the same filtering crop
+        cy.get("[data-cy=h1]>[data-cy=r0]").should("have.text","ARUGULA")
+        cy.get("[data-cy=h1]>[data-cy=r1]").should("have.text","ARUGULA")
+        cy.get("[data-cy=h1]>[data-cy=r2]").should("have.text","ARUGULA")
+
+    })    
   })
