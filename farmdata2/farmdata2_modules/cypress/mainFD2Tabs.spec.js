b2d88e16ff108f682d4bc4b56c544878a9b3f0a0
gfr4d3a2 <gfr4d3a2@example.com>
2023-11-17 13:16:21 -0500

    Add tests to validate that tabs that shouldn't appear to guests do not. Refactor comments to be less verbose.

--- farmdata2/farmdata2_modules/cypress/mainFD2Tabs.spec.js
+++ farmdata2/farmdata2_modules/cypress/mainFD2Tabs.spec.js
@@ -1,43 +1,56 @@
 /** This tests that When a worker is logged in the 
  * UI should contain the FieldKit and BarnKit tabs but not the FD2Config tab.*/
 describe("Testing worker is logged in", () => {
-    // should contain the FieldKit
+    //managers should see all tabs
     it("check contain the FieldKit manager1", () => {
         cy.login("manager1", "farmdata2")
         cy.visit("/farm")
-        cy.get('.tabs--primary > :nth-child(4) > .glyphicons-processed').should("have.contain","FieldKit")
+        cy.get('.tabs--primary > :nth-child(4) > .glyphicons-processed').should("have.contain", "FieldKit")
     })
-    // should contain the BarnKit
     it("check contain the BarnKit manager1", () => {
         cy.login("manager1", "farmdata2")
         cy.visit("/farm")
-        cy.get('.tabs--primary > :nth-child(5) > .glyphicons-processed').should("have.contain","BarnKit")
+        cy.get('.tabs--primary > :nth-child(5) > .glyphicons-processed').should("have.contain", "BarnKit")
     })
-    // should contain the FD2Config
     it("check contain theFD2Config manager1", () => {
         cy.login("manager1", "farmdata2")
         cy.visit("/farm")
-        cy.get('.tabs--primary > :nth-child(8) > .glyphicons-processed').should("have.contain","FD2 Config")
+        cy.get('.tabs--primary > :nth-child(8) > .glyphicons-processed').should("have.contain", "FD2 Config")
         //cy.get('.tabs--primary').should("contain","FD2Config")
     })
-    
-    // should contain the FieldKit
+
+    //workers should see all tabs except for fd2config
     it("check contain the FieldKit worker1", () => {
         cy.login("worker1", "farmdata2")
         cy.visit("/farm")
-        cy.get('.tabs--primary > :nth-child(4) > .glyphicons-processed').should("have.contain","FieldKit")
+        cy.get('.tabs--primary > :nth-child(4) > .glyphicons-processed').should("have.contain", "FieldKit")
     })
-    // should contain the BarnKit
     it("check contain the BarnKit worker1", () => {
         cy.login("worker1", "farmdata2")
         cy.visit("/farm")
-        cy.get('.tabs--primary > :nth-child(5) > .glyphicons-processed').should("have.contain","BarnKit")
+        cy.get('.tabs--primary > :nth-child(5) > .glyphicons-processed').should("have.contain", "BarnKit")
     })
-    // should not contain the FD2Config
     it("check not contain theFD2Config worker1", () => {
         cy.login("worker1", "farmdata2")
         cy.visit("/farm")
-        cy.get('.tabs--primary').should("not.contain","FD2Config")
+        cy.get('.tabs--primary').should("not.contain", "FD2Config")
     })
-  
-    })    
+
+    //guests should not see any tabs
+    it("check contain the FieldKit worker1", () => {
+        cy.login("guest", "farmdata2")
+        cy.visit("/farm")
+        cy.get('.tabs--primary').should("not.contain", "FieldKit")
+    })
+    it("check contain the BarnKit worker1", () => {
+        cy.login("guest", "farmdata2")
+        cy.visit("/farm")
+        cy.get('.tabs--primary').should("not.contain", "BarnKit")
+    })
+    it("check not contain theFD2Config worker1", () => {
+        cy.login("guest", "farmdata2")
+        cy.visit("/farm")
+        cy.get('.tabs--primary').should("not.contain", "FD2Config")
+    })
+
+})
